e03acd1e9359eedc82622c2d8e77c88aa2bd29ea
Sheila Diz <dizsheila07@gmail.com>
2024-10-07 16:47:52 -0300

    refactor: move reusable objects to util folder

---
+++ src/utils/statusOptions.ts
@@ -0,0 +1,7 @@
+export const statusOptions = [
+    { value: "ACCEPTED", label: "Aceptado" },
+    { value: "PENDING", label: "Pendiente" },
+    { value: "EXPIRED", label: "Expirado" },
+    { value: "FAILED", label: "Fallido" },
+    { value: "COMPLETED", label: "Completado" },
+];
